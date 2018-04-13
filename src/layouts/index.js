51c17fd1e0f3a45da6c63475e21856a9b1121497
johnrieth <john.rieth@outlook.com>
2018-04-12 20:40:02 -0700

    new stuff

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Erhaben Research" />
     <Navbar />
     <div>{children()}</div>
   </div>
