77b7bc4962863e29bf3a85c6c86d2dd430a3dcec
johnrieth <john.rieth@outlook.com>
2018-04-16 15:57:49 -0700

    reboot

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,21 +9,23 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <h1>
-            Erhaben Research
-          </h1>
+          <figure className="image">
+            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          </figure>
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
           About
         </Link>
-       
+        <Link className="navbar-item" to="/products">
+          Products
+        </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/johnrieth/erhaben"
+          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
           target="_blank"
           rel="noopener noreferrer"
         >
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Erhaben Research" />
+    <Helmet title="Home | Gatsby + Netlify CMS" />
     <Navbar />
     <div>{children()}</div>
   </div>
